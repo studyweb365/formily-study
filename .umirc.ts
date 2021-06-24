4bb14d19110d0835d83f0d2d00ce9bc5f2bb9eb0
sunpanyu <996251389@qq.com>
2021-06-24 19:14:46 +0800

    第一次提交代码

---
+++ .umirc.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+
+  // 配置babel-plugin-import，让在使用@formily/antd按需加载，保证样式不丢失
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      { libraryName: 'antd', libraryDirectory: 'lib', style: true },
+      'antd',
+    ],
+    [
+      'babel-plugin-import',
+      { libraryName: '@formily/antd', libraryDirectory: 'lib', style: true },
+    ],
+  ],
+  dynamicImport: {},
+  publicPath: '/formily-form/', // 设置静态文件路径
+  history: { type: 'hash' },
+});
